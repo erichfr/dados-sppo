69f5651f1405b07b5925957c66abbe37f4d756cb
erichfr <erichfr1@hotmail.com>
2022-06-21 19:05:01 -0300

    first commit

---
+++ src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/estuturaMvc/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = '';
+    const DB_DATABASE = '';
+    CONST DB_USER = '';
+    const DB_PASS = '';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
