069c5f8865ea98d47a4d68a8739de950fac6642c
Erich Fournier <erichfr1@hotmail.com>
2022-07-05 16:53:50 -0300

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -5,9 +5,9 @@ class Config {
     const BASE_DIR = '/estuturaMvc/public';
 
     const DB_DRIVER = 'mysql';
-    const DB_HOST = 'localhost';
-    const DB_DATABASE = 'validacao_db';
-    CONST DB_USER = 'root';
+    const DB_HOST = '';
+    const DB_DATABASE = '';
+    CONST DB_USER = '';
     const DB_PASS = '';
 
     const ERROR_CONTROLLER = 'ErrorController';
